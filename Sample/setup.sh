493728b8951509e14d96227744556b70648b8091
enzo corsiero <>
2025-05-06 22:33:24 +0200

    Improve setup.sh with mise

--- Sample/setup.sh
+++ Sample/setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# Install Mise, if not installed.
+if ! brew ls --versions mise > /dev/null 2>&1; then
+  echo "mise not found in Homebrew, installing..."
+  brew install mise
+fi
+
 # Install Tuist, if not installed.
-if ! command -v tuist &> /dev/null
-then
-    echo "Tuist not found, installing..."
-    curl -Ls https://install.tuist.io | bash
+if ! command -v tuist &> /dev/null; then
+  echo "tuist not found, installing via mise..."
+  mise install tuist
 fi
 
 # Delete current Xcode Project.
 rm -rf $PROJECT_NAME.xcodeproj
 
 # Generate the tuist project.
-tuist install
-tuist generate
+mise x -- tuist install
+mise x -- tuist generate

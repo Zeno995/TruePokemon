bb22af528f07847e91a86a72cb07f88ed78c3804
Enzo <e.corsiero@dstech.it>
2025-05-06 16:34:39 +0200

    Install tuist if not present in setup.sh

--- Sample/setup.sh
+++ Sample/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install Tuist, if not installed.
+if ! command -v tuist &> /dev/null
+then
+    echo "Tuist not found, installing..."
+    curl -Ls https://install.tuist.io | bash
+fi
+
 # Delete current Xcode Project.
 rm -rf $PROJECT_NAME.xcodeproj
 

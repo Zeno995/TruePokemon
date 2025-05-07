47ec4248d22c90f8145eaa6063607b90e949044b
Enzo <e.corsiero@dstech.it>
2025-05-07 12:46:23 +0200

    Fix correct derived data.

--- Sample/Derived/Sources/TuistBundle+TruePokemonSampleApp.swift
+++ Sample/Derived/Sources/TuistBundle+TruePokemonSampleApp.swift
@@ -1,18 +1,12 @@
-// periphery:ignore:all
-// swiftlint:disable:this file_name
 // swiftlint:disable all
 // swift-format-ignore-file
 // swiftformat:disable all
-#if hasFeature(InternalImportsByDefault)
-public import Foundation
-#else
 import Foundation
-#endif
 // MARK: - Swift Bundle Accessor for Frameworks
 private class BundleFinder {}
 extension Foundation.Bundle {
 /// Since TruePokemonSampleApp is a application, the bundle for classes within this module can be used directly.
-    static let module = Bundle(for: BundleFinder.self)
+static let module = Bundle(for: BundleFinder.self)
 }
 // MARK: - Objective-C Bundle Accessor
 @objc
